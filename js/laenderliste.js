9f3aaa9d4702ce1c6732e140140bf7b1889b6006
lsi <siebertl@bzz.ch>
2023-01-19 23:21:16 +0100

    bugfix script.js zeile 16 selector

--- js/laenderliste.js
+++ js/laenderliste.js
@@ -1,7 +1,16 @@
 //JavaScript Objekt von Länderliste
 let laenderliste = {
-    "CHF" : "CH",
-    "EUR" : "FR",
-    "USD" : "US",
+    "CH" : "CHF",
+    "EU" : "EUR",
+    "US" : "USD",
 
 }
+let flaggen = {
+    "CH" : "🇨🇭",
+    "EU" : "🇩🇪",
+    "US" : "🇺🇸",
+}
+
+const franken = flaggen.CH + " " + laenderliste.CH
+const euro = flaggen.EU + " " + laenderliste.EU
+const dollar = flaggen.US + " " + laenderliste.US
